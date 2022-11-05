16e08348558eb1c9bce4e65e340a42a239bda4bf
winter_x64 <75623356+winter-x64@users.noreply.github.com>
2022-11-05 21:24:09 +0530

    new commit

--- main.py
+++ main.py
@@ -18,6 +18,34 @@ async def status(ctx):
 
 
 
+# .........................[fun cmd]..........................
+# -------------------------[to emoji]-------------------------
+@client.slash_command(name= "toemoji", description= "convert text to emoji")
+async def toemoji(self, interaction: Interaction, msg: str):
+    emojis = []     #<- empty array>
+    # gets all the letter in the msg
+    for s in msg.lower():
+        if s.isalpha():
+            emojis.append(f":regional_indicator_{s}:")
+        else:
+            emojis.append(s)
+    await interaction.response.send_message(' '.join(emojis))
+
+# -------------------------[coin]-------------------------------
+@client.slash_command(name= "flipcoin", description= "coin flip")
+async def coin(self, interaction: Interaction):
+    coinlist = ["heads", "tails"]   #<- coin list>
+    embed = nextcord.Embed(title="coin", description= f"coin => {random.choice(coinlist)}")
+    await interaction.response.send_message(embed= embed)
+
+# -------------------------[choose]-----------------------------
+@client.slash_command(name= "choose", description="choose a person")
+async def choose(self, interaction: Interaction, limit: int):
+    chosenPerson = random.randint(1, limit)
+    embed = nextcord.Embed(title= "God will choose!!!")
+    embed.add_field(name= f"God ", value = f"=> {chosenPerson}")
+    await interaction.response.send_message(embed = embed)
+
 # ----------------------[ run ]------------------------
 try:
     client.run(os.getenv("TOKEN"))
